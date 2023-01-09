2237b899a8e3e11faaeb41eb9ee1aa27a033a448
paxom4ik4 <pasha.zelenko001@gmail.com>
2023-01-09 18:21:51 +0300

    Trifecta 333

--- src/app/components/newbie/newbie.js
+++ src/app/components/newbie/newbie.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './newbie.scss';
-import newbie from './newbie.png';
+import firstDay from '../../assets/firstDay.png';
+import secondDay from '../../assets/secondDay.png';
+import thirdDay from '../../assets/trirdDay.png';
 import { useEffect } from "react";
 import {useNavigate} from "react-router-dom";
 
@@ -34,24 +36,24 @@ export const Newbie = ({ isVerified }) => {
         <div className={`${DEFAULT_CLASSNAME}_wrapper`}>
             <div className={DEFAULT_CLASSNAME}>
                 <div className={`${DEFAULT_CLASSNAME}_item`} onClick={() => openLink("https://t.me/+GgNg39XQR5M4ZGJi")}>
-                    <img src={newbie} alt={'video'} />
+                    <img src={firstDay} alt={'video'} />
                     <div className={`${DEFAULT_CLASSNAME}_item_text`}>
-                        <div className={`${DEFAULT_CLASSNAME}_item_title`}>{"День 1"}</div>
-                        <div className={`${DEFAULT_CLASSNAME}_item_text_content`}>{"Описание ролика (Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer tookLetraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.)"}</div>
+                        <div className={`${DEFAULT_CLASSNAME}_item_title`}>{"День первый"}</div>
+                        <div className={`${DEFAULT_CLASSNAME}_item_text_content`}>{"Начинаем обучение - https://t.me/+GgNg39XQR5M4ZGJi"}</div>
                     </div>
                 </div>
                 <div className={`${DEFAULT_CLASSNAME}_item`} onClick={() => openLink("https://t.me/+8YUx4cEdQ49hOWYy")}>
-                    <img src={newbie} alt={'video'} />
+                    <img src={secondDay} alt={'video'} />
                     <div className={`${DEFAULT_CLASSNAME}_item_text`}>
-                        <div className={`${DEFAULT_CLASSNAME}_item_title`}>{"День 2"}</div>
-                        <div className={`${DEFAULT_CLASSNAME}_item_text_content`}>{"Описание ролика (Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer tookLetraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.)"}</div>
+                        <div className={`${DEFAULT_CLASSNAME}_item_title`}>{"День второй"}</div>
+                        <div className={`${DEFAULT_CLASSNAME}_item_text_content`}>{"Продолжаем обучение - https://t.me/+8YUx4cEdQ49hOWYy"}</div>
                     </div>
                 </div>
                 <div className={`${DEFAULT_CLASSNAME}_item`} onClick={() => openLink("https://t.me/+Fn5Of3OrNJFjNGRi")}>
-                    <img src={newbie} alt={'video'} />
+                    <img src={thirdDay} alt={'video'} />
                     <div className={`${DEFAULT_CLASSNAME}_item_text`}>
-                        <div className={`${DEFAULT_CLASSNAME}_item_title`}>{"День 3"}</div>
-                        <div className={`${DEFAULT_CLASSNAME}_item_text_content`}>{"Описание ролика (Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer tookLetraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.)"}</div>
+                        <div className={`${DEFAULT_CLASSNAME}_item_title`}>{"День третий"}</div>
+                        <div className={`${DEFAULT_CLASSNAME}_item_text_content`}>{"Заканчиваем обучение - https://t.me/+Fn5Of3OrNJFjNGRi"}</div>
                     </div>
                 </div>
             </div>

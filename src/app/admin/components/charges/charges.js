98c68e655716ffcd93074cc437cd9c6b90df64c3
Pavel Zelenko <pasha.zelenko001@gmail.com>
2024-09-25 13:07:02 +0300

    feat: small bug fix and improvements v3

--- src/app/admin/components/charges/charges.js
+++ src/app/admin/components/charges/charges.js
@@ -124,7 +124,7 @@ export const AdminCharges = () => {
                             <div className={`charges_table_item`} style={{ pointerEvents: (item.selected) && "none", opacity: (item.selected) && '0.5'}}>
                                 <div className={`charges_table_item_select ${item.selected && 'confirmed'} ${!!selectedCharges.includes(item.id) && 'selected'}`} onClick={() => chargeHandler(item)}/>
                                 <div>{item.accuralName}</div>
-                                <div>{item.userEmail}</div>
+                                <div>{`${item.userFirstName} ${item.userLastName}`}</div>
                                 <Tooltip text={item.referralName}><div>{item.referralName}</div></Tooltip>
                                 <div>{item.accuralPercent}</div>
                                 <div>{item.initialAmount  + ' / ' + item.initialAmountByn}</div>
